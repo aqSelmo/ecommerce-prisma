db392d84bedb491b03f4814ca60be2ac2e1b4072
Guilherme Falcao <guilusa25@gmail.com>
2021-05-11 22:21:47 -0300

    Inciando testes, integra

--- src/__tests__/users.spec.ts
+++ src/__tests__/users.spec.ts
@@ -2,4 +2,5 @@ import supertest from 'supertest';
 
 describe('test the entire routes about users', () => {
   it('should returns an array of users', () => {});
+  it('should returns an array of users', () => {});
 });
